2e9f6445339c026dd8121371c7b0ac8cb816194d
Luziane <freitas.lu@outlook.com>
2019-10-09 00:58:20 -0300

    Adicionado components
    Adicionando barra de navegação com menus e cards para postagens.
    
    OBSERVAÇÃO: Formatar elementos

--- app1/src/app/app.module.ts
+++ app1/src/app/app.module.ts
@@ -3,14 +3,31 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopoComponent } from './topo/topo.component';
+import { PainelComponent } from './painel/painel.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+
+import {MatButtonModule} from '@angular/material/button';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatCardModule} from '@angular/material/card';
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopoComponent,
+    PainelComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ app1/src/app/topo/topo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core'
+
+@Component({
+    // Indica o rotulo utilizado pela instancia deste componente dentros dos templates de outros componentes
+    selector:'app-topo',
+    templateUrl: './topo.component.html',
+    styleUrls: ['./topo.component.css']
+})
+
+export class TopoComponent {
+
+}
