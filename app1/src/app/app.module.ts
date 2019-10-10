bd48bfc225ac0d03e19fa2168fedc1ae77784001
Giovanna Taglialegna Verola Moreira <giovanna.verola2016@gmail.com>
2019-10-10 17:45:21 -0300

    Ajustes nas telas de login e cadastro
    Ajustes das rotas nas telas iniciais
    Co-authored-by: Gabriela Garcia<gabii6431@gmail.com>

--- app1/src/app/app.module.ts
+++ app1/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { TopoComponent } from './topo/topo.component';
 import { PainelComponent } from './painel/painel.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { Routes, RouterModule } from '@angular/router';
+import {ROUTES} from './app.routes'
 
 import {MatButtonModule} from '@angular/material/button';
 import {MatMenuModule} from '@angular/material/menu';
@@ -15,6 +16,7 @@ import {MatCardModule} from '@angular/material/card';
 import {MatInputModule} from '@angular/material/input';
 import { LoginComponent } from './login/login.component';
 import { CadastroComponent } from './cadastro/cadastro.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent },
@@ -29,13 +31,14 @@ const routes: Routes = [
     
   ],
   imports: [
-    RouterModule.forRoot(routes),
+    RouterModule.forRoot(ROUTES),
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatButtonModule,
     MatMenuModule,
     MatToolbarModule,
+    MatFormFieldModule,
     MatCardModule,
     MatInputModule
   ],
---
+++ app1/src/app/app.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router'
+
+import {PainelComponent} from './painel/painel.component'
+import {LoginComponent} from './login/login.component'
+import {CadastroComponent} from './cadastro/cadastro.component'
+
+export const ROUTES: Routes = [
+    {path: "", component: LoginComponent},
+    {path: 'cadastro', component: CadastroComponent},
+    {path: 'painel', component: PainelComponent},
+]
